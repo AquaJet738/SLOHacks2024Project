054f20051dfe68d0a16fff2b026a077fb83dd474
Steven Wong <stevenwong2726@gmail.com>
2025-03-25 16:09:17 -0700

    removed duplicate files

--- build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Update package list and install dependencies
-apt-get update
-apt-get install -y libxml2-dev libxslt-dev
-
-# Proceed with the regular pip install
-pip install -r requirements.txt
-
